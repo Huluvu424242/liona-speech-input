2d37e019b3f07cc2af6faa612664d019195e28b6
Huluvu424242 <huluvu424242@gmail.com>
2019-09-03 21:51:41 +0200

    Create speech-output.js

---
+++ speech-output.js
@@ -0,0 +1,32 @@
+var synthese = window.speechSynthesis;
+var suchfeld = document.querySelector('#suchfeld');
+
+function suchkriterienVorlesen(){
+    if (synthese.speaking) {
+        console.error('Spricht bereits');
+        return;
+    }
+    if (suchfeld.value !== '') {
+        var vorleseText = 'Ihre Eingaben zur Kontrolle: '+ suchfeld.value;
+        console.log("Text:"+vorleseText);
+        var leserStimmeMitText = new SpeechSynthesisUtterance(vorleseText);
+        leserStimmeMitText.onend = function (event) {
+            console.log('Vorlesen beendet');
+        }
+        leserStimmeMitText.onerror = function (event) {
+            console.error('Fehler beim Vorlesen');
+        }
+        var voices = synthese.getVoices();
+        for(i = 0; i < voices.length ; i++) {
+            if(voices[i].default) {
+                console.log("Voice:"+voices[i].name + voices[i].lang);
+                leserStimmeMitText.voice = voices[i];
+                break;
+            }
+        }
+        leserStimmeMitText.pitch = 1;
+        leserStimmeMitText.rate = 1;
+        leserStimmeMitText.volume = 1;
+        synthese.speak(leserStimmeMitText);
+    }
+}
