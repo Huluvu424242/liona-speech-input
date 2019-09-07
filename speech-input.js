2a9af9327571eb58ec0fcb72e255e2d05662fba7
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-09-07 13:47:29 +0200

    Refactoring

--- speech-input.js
+++ speech-input.js
@@ -1,70 +1,63 @@
 'use strict';
 
+import {Logger} from './log-helper'
+
 const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-let shadowRoot;
-let eingabeFeldId;
-let words;
-let recognition;
-
 class Spracheingabe {
 
     constructor(shadowRootRef, inputFieldIdRef, wortliste) {
-        shadowRoot = shadowRootRef;
-        eingabeFeldId = inputFieldIdRef;
-        words = wortliste;
-        recognition = this.initialisiereSpracherkennung();
-        this.logMessage('####shadow root:' + shadowRoot);
-        this.logMessage('####eingabe feld id:' + eingabeFeldId);
+        this.shadowRoot = shadowRootRef;
+        this.eingabeFeldId = inputFieldIdRef;
+        this.words = wortliste;
+        this.recognition = this.initialisiereSpracherkennung();
+        Logger.logMessage('####shadow root:' + this.shadowRoot);
+        Logger.logMessage('####eingabe feld id:' + this.eingabeFeldId);
 
     }
 
     initialisiereSpracherkennung() {
         // Spracherkennung konfigurieren
-        const recognition = new SpeechRecognition();
-        recognition.lang = 'de-DE';
-        recognition.interimResults = false;
-        recognition.maxAlternatives = 1;
+        const speechRecognition = new SpeechRecognition();
+        speechRecognition.lang = 'de-DE';
+        speechRecognition.interimResults = false;
+        speechRecognition.maxAlternatives = 1;
         // Grammatik initialisieren
-        if (words) {
+        if (this.words) {
             const speechRecognitionList = new SpeechGrammarList();
-            const grammar = '#JSGF V1.0; grammar words; public <word> = ' + words.join(' | ') + ' ;';
+            const grammar = '#JSGF V1.0; grammar words; public <word> = ' + this.words.join(' | ') + ' ;';
             speechRecognitionList.addFromString(grammar, 1);
-            recognition.grammars = speechRecognitionList;
+            speechRecognition.grammars = speechRecognitionList;
         }
-        return recognition;
+        return speechRecognition;
     }
 
     erkenneSprachEingabe(callback) {
         // Spracherkennung starten
-        recognition.start();
-        this.logMessage('###speaker:' + recognition);
+        this.recognition.start();
+        Logger.logMessage('###speaker:' + this.recognition);
 
         // Callbacks implementieren
-        const eingabeFeld = shadowRoot.getElementById(eingabeFeldId);
-        this.logMessage('####bevor this.eingabefeld:' + eingabeFeld.outerHTML);
-        const objekt = this;
-        recognition.onresult =  () => {
-            this.logMessage('####eingabefeld:' + eingabeFeld.outerHTML);
+        const eingabeFeld = this.shadowRoot.getElementById(this.eingabeFeldId);
+        Logger.logMessage('####bevor this.eingabefeld:' + eingabeFeld.outerHTML);
+        this.recognition.onresult = (event) => {
+            Logger.logMessage('####eingabefeld:' + eingabeFeld.outerHTML);
             const speechResult = event.results[0][0].transcript;
             eingabeFeld.value = speechResult;
-            this.logMessage('####eingabefeld changed:' + eingabeFeld.outerHTML);
-            this.logMessage('Confidence: ' + event.results[0][0].confidence);
+            Logger.logMessage('####eingabefeld changed:' + eingabeFeld.outerHTML);
+            Logger.logMessage('Confidence: ' + event.results[0][0].confidence);
             callback(speechResult);
-        }
+        };
 
-        recognition.onspeechend = function () {
-            recognition.stop();
+        this.recognition.onspeechend = () => {
+            this.recognition.stop();
         }
 
 
     }
 
-    logMessage(message) {
-        console.log(message);
-    }
-};
+}
 
 export {Spracheingabe};
